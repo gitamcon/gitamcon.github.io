43145c7644ca33350365af2a0a985ceef75f307a
gitamcon <73210703+gitamcon@users.noreply.github.com>
2021-06-10 00:41:58 +0530

    Add files via upload

---
+++ audio.js
@@ -0,0 +1,61 @@
+var x1 = document.getElementById("myAudio1");
+var x2 = document.getElementById("myAudio2");
+var x3 = document.getElementById("myAudio3");
+var x4 = document.getElementById("myAudio4");
+var x5 = document.getElementById("myAudio5");
+var x6 = document.getElementById("myAudio6");
+var x7 = document.getElementById("myAudio7");
+var x8 = document.getElementById("myAudio8");
+var x9 = document.getElementById("myAudio9");
+var x10 = document.getElementById("myAudio10");
+var x11 = document.getElementById("myAudio11");
+var x12 = document.getElementById("myAudio12");
+
+
+function playAudio1() {
+    x1.play();
+}
+
+function playAudio2() {
+    x2.play();
+}
+
+function playAudio3() {
+    x3.play();
+}
+
+function playAudio4() {
+    x4.play();
+}
+
+function playAudio5() {
+    x5.play();
+}
+
+function playAudio6() {
+    x6.play();
+}
+
+function playAudio7() {
+    x7.play();
+}
+
+function playAudio8() {
+    x8.play();
+}
+
+function playAudio9() {
+    x9.play();
+}
+
+function playAudio10() {
+    x10.play();
+}
+
+function playAudio11() {
+    x11.play();
+}
+
+function playAudio12() {
+    x12.play();
+}
